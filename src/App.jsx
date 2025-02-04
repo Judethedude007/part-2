4df0f21a3bee23ffe6e9d60259de7af3cccac754
jude thomas <judethomas2004.com>
2025-02-04 12:44:12 +0530

    2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]); 
+  const [persons, setPersons] = useState([]); 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    console.log('New name state changed:', newName);
-  }, [newName]);
+    axios
+      .get('http://localhost:3002/persons') // Change the port to 3002
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName, number: newNumber }]);
+      const newPerson = { name: newName, number: newNumber, id: persons.length + 1 };
+      setPersons([...persons, newPerson]);
       setNewName('');
       setNewNumber('');
     }
