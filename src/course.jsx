69900d4c6087c9825df501d140be6a7bdde569f2
jude thomas <judethomas2004.com>
2025-02-04 10:36:38 +0530

    2.4

--- src/course.jsx
+++ src/course.jsx
@@ -1,28 +1,79 @@
 import PropTypes from 'prop-types';
 
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
+  )
+}
+
 const Course = ({ course }) => {
   const totalExercises = course.parts.reduce(function (sum, part) {
     console.log(`Adding ${part.exercises} exercises from ${part.name}`);
     return sum + part.exercises;
   }, 0);
 
-  console.log(`Total exercises (excluding Redux): ${totalExercises}`);
-  console.log(`Total exercises (including Redux): ${totalExercises + 11}`);
+  console.log(`Total exercises: ${totalExercises}`);
 
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
       <ul>
         {course.parts.map(part => (
           <li key={part.id}>
             {part.name} {part.exercises}
           </li>
         ))}
-        <li key="redux">
-          Redux 11
-        </li>
       </ul>
-      <p>Total exercises: {totalExercises + 11}</p>
+      <p><strong>Total exercises: {totalExercises}</strong></p>
     </div>
   );
 };
@@ -40,5 +91,5 @@ Course.propTypes = {
   }).isRequired
 };
 
+export { App, Course };
 export default Course;
-
